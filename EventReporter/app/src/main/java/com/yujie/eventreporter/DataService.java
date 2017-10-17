be482410b6154a4ea91bc76f3eb43f76fe535d33
yujie cao <yujiecao@yujies-MacBook-Pro.local>
2017-10-16 17:27:20 -0700

    first commit

---
+++ EventReporter/app/src/main/java/com/yujie/eventreporter/DataService.java
@@ -0,0 +1,25 @@
+package com.yujie.eventreporter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yujiecao on 10/3/17.
+ */
+
+public class DataService {
+    /**
+     * Fake all the event data for now. We will refine this and connect
+     * to our backend later.
+     */
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<Event>();
+        for (int i = 0; i < 10; ++i) {
+            eventData.add(
+                    new Event("Event", "1184 W valley Blvd, CA 90101",
+                            "This is a huge event"));
+        }
+        return eventData;
+    }
+
+}
